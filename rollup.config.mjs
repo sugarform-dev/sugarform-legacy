37a812bca7b243964718fd05a60132df3478f7c2
AsPulse <aspulse.x@gmail.com>
2023-03-05 14:46:42 +0900

    Build with SWC

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,7 @@
 /* eslint-disable no-restricted-exports */
 import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
+import { swc, defineRollupSwcOption } from 'rollup-plugin-swc3';
 import packageJson from './package.json' assert { type: 'json' };
 
 export default [
@@ -11,22 +11,21 @@ export default [
       {
         file: packageJson.main,
         format: 'cjs',
-        sourcemap: true,
+        sourcemap: false,
       },
       {
         file: packageJson.module,
         format: 'esm',
-        sourcemap: true,
+        sourcemap: false,
       },
     ],
     plugins: [
       commonjs({
         include: [ 'node_modules/**' ],
       }),
-      typescript({
-        tsconfig: 'tsconfig.json',
-        exclude: [ '**/__tests__/**' ],
-      }),
+      swc(defineRollupSwcOption({
+        exclude: /__tests__/,
+      })),
     ],
     external: [ 'react', 'react-dom' ],
   },
