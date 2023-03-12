43c6f24f034f25f7cbff4b402bf426db5a96b1c0
AsPulse <aspulse.x@gmail.com>
2023-03-12 12:21:34 +0900

    Add test should work with ommited args

--- tests/useSugarForm.test.ts
+++ tests/useSugarForm.test.ts
@@ -3,6 +3,7 @@ import { act, renderHook } from '@testing-library/react';
 import type { Sugar, SugarValue } from '../src/component/sugar';
 import { useSugarForm } from '../src/component/sugarform';
 
+// eslint-disable-next-line max-lines-per-function
 describe('useSugarForm', () => {
 
   it('should work', () => {
@@ -11,6 +12,26 @@ describe('useSugarForm', () => {
 
     const { result: { current: { fields: { a } } } } =
       renderHook(() => sugar.useObject({}));
+
+    expect(a.template).toBe('foo');
+
+    a.mounted = true;
+    (a as Sugar<string> & { mounted: true }).get = (): SugarValue<string> => ({ success: true, value: 'foo' });
+
+    expect(render()).toStrictEqual({
+      success: true,
+      value: {
+        a: 'foo',
+      },
+    });
+  });
+
+  it('should work with ommited args', () => {
+    const { result: { current: { sugar, render } } } =
+      renderHook(() => useSugarForm<{ a: string }>({ defaultValue: { a: 'foo' } }));
+
+    const { result: { current: { fields: { a } } } } =
+      renderHook(() => sugar.useObject());
 
     expect(a.template).toBe('foo');
 
