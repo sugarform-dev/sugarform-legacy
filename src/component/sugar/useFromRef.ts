c1e2af22c3273f0897278521b5fd049e371c4945
AsPulse <aspulse.x@gmail.com>
2023-03-12 23:42:05 +0900

    [feat] ref for defaultvalue

--- src/component/sugar/useFromRef.ts
+++ src/component/sugar/useFromRef.ts
@@ -1,11 +1,13 @@
+import type { MutableRefObject } from 'react';
+import { useRef } from 'react';
 import type { Sugar, SugarValue } from '.';
 import { SugarFormError } from '../../util/error';
 import { setDirty } from './dirty';
 
 export function useSugarFromRef<T>(
-  sugar: Sugar<T>, param: { get: () => SugarValue<T>, set: (value: T) => void },
+  sugar: Sugar<T>, param: { get: () => SugarValue<T>, set: (value: T) => boolean },
 ): {
-  onChange: () => void, onBlur: () => void
+  onChange: () => void, onBlur: () => void, defaultValueRef: MutableRefObject<T | undefined>,
 } {
 
   const refreshDirty = (): void => {
@@ -14,11 +16,19 @@ export function useSugarFromRef<T>(
     setDirty(sugar, !value.success || sugar.template !== value.value);
   };
 
+  const defaultValue = useRef<T | undefined>(undefined);
+
+  const setterWithDefault = (v: T): void => {
+    if (!param.set(v)) {
+      defaultValue.current = v;
+    }
+  };
+
   const updateSugar = sugar as Sugar<T> & { mounted: true };
   updateSugar.mounted = true;
   updateSugar.get = param.get;
   updateSugar.set = (v): void => {
-    param.set(v);
+    setterWithDefault(v);
     refreshDirty();
   };
   updateSugar.setTemplate = (v): void => {
@@ -27,12 +37,13 @@ export function useSugarFromRef<T>(
   };
   updateSugar.isDirty = false;
 
-  param.set(sugar.template);
+  setterWithDefault(sugar.template);
   return {
     onChange: (): void => {
       if (!sugar.mounted) throw new SugarFormError('SF0021', `Path: ${sugar.path}}`);
       if (!sugar.isDirty) setDirty(sugar, true);
     },
-    onBlur: () => refreshDirty(),
+    onBlur: refreshDirty,
+    defaultValueRef: defaultValue,
   };
 }
