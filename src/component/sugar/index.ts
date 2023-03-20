e1958df4a8ba983d8c062710b8543a8cffe88357
AsPulse <aspulse.x@gmail.com>
2023-03-20 20:24:37 +0900

    Add UseArray interfaces

--- src/component/sugar/index.ts
+++ src/component/sugar/index.ts
@@ -25,12 +25,8 @@ export interface SugarData<T> {
   },
   use: <U extends SugarObject>(options: SugarUserReshaper<T, U>) => SugarObjectNode<U>,
   useObject: T extends SugarObject ? (options?: SugarUser<T>) => SugarObjectNode<T> : never;
+  useArray: T extends Array<infer U> ? (options?: SugarArrayUser) => SugarArrayNode : never;
 }
-// ) & (
-//   T extends Array<infer U> ?
-//     { useArray: (options: SugarUserArray<U>) => SugarArrayNode<U> } :
-//     Record<string, never>
-// );
 
 export type SugarValue<T> = {
   success: true,
@@ -55,10 +51,10 @@ export interface SugarObjectNode<U extends SugarObject> {
   fields: { [K in keyof U]: Sugar<U[K]> },
 }
 
-// export interface SugarUserArray<T> {
-//
-// }
-//
-// export interface SugarArrayNode<T> {
-//
-// }
+export interface SugarArrayUser {
+
+}
+
+export interface SugarArrayNode {
+
+}
