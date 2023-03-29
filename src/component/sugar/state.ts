e2d0912509a9e0a44e49949860d85d52e4b69cc5
AsPulse <84216737+AsPulse@users.noreply.github.com>
2023-03-29 14:04:59 +0900

    [fix] useState isDirty gets crazy (#48)

--- src/component/sugar/state.ts
+++ src/component/sugar/state.ts
@@ -1,6 +1,8 @@
 import type { Dispatch, SetStateAction } from 'react';
+import { useRef } from 'react';
 import type { SugarValue } from '.';
 import type { Sugar } from '../..';
+import { debug } from '../../util/logger';
 import { isSugarObject } from '../../util/object';
 import { setDirty } from './dirty';
 
@@ -11,28 +13,31 @@ export function useStateFollower<T>(
   comparator: (a: T, b: T) => boolean = (a, b): boolean => a === b,
 ): void {
 
+  const mountedRef = useRef(false);
   const fixedState = isSugarObject(state) ? { ...state } : state;
   const mountedSugar = sugar as Sugar<T> & { mounted: true };
 
-  if (sugar.mounted) {
-    const oldValue = sugar.get();
-    if (oldValue.success && !comparator(fixedState, oldValue.value)) {
-      setDirty(sugar, comparator(sugar.template, fixedState));
-    }
+  if (mountedRef.current && sugar.mounted) {
+    setDirty(sugar, !comparator(sugar.template, fixedState));
   }
 
   mountedSugar.get = (): SugarValue<T> => ({ success: true, value: fixedState });
   mountedSugar.set = (value: T): void => setState(value);
   mountedSugar.setTemplate = (template: T): void => {
     sugar.template = template;
-    setTimeout(() => setState(template), 0);
+    setState(template);
   };
 
-  if (!sugar.mounted) {
+  if (!mountedRef.current && sugar.mounted) {
+    debug('WARN', `Sugar is already mounted, but refs are not initialized. Remounting... Path: ${sugar.path}`);
+    mountedRef.current = false;
+  }
+  if (!mountedRef.current) {
     mountedSugar.mounted = true;
     mountedSugar.isDirty = false;
     mountedSugar.setTemplate(sugar.template);
     mountedSugar.upstream.fire('mounted', {});
+    mountedRef.current = true;
   }
 
 }
