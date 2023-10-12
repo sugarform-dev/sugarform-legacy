2a49c5813996d10fa09197a419d3112810ec0686
AsPulse <aspulse.x@gmail.com>
2023-10-12 15:26:05 +0900

    Add resetDirty

--- packages/core/src/component/sugar/dirty.ts
+++ packages/core/src/component/sugar/dirty.ts
@@ -7,3 +7,8 @@ export function setDirty<T>(sugar: Sugar<T>, isDirty: boolean): void {
     sugar.upstream.fire('updateDirty', { isDirty });
   });
 }
+
+export function resetDirty<T>(sugar: Sugar<T> & { mounted: true }): void {
+  sugar.isDirty = false;
+  sugar.upstream.fire('updateDirty', { isDirty: false });
+}
