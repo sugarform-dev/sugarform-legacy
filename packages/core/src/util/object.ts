3eb2766eb67dc8afc334415228bd9cee6453a947
AsPulse <aspulse.x@gmail.com>
2023-11-12 17:11:45 +0900

    Add merge util

--- packages/core/src/util/object.ts
+++ packages/core/src/util/object.ts
@@ -1,6 +1,19 @@
+import type { SetTemplateMode } from '@/component/sugar';
+
 export type SugarObject = Record<string, unknown>;
 export function isSugarObject(obj: unknown): obj is SugarObject {
   return obj?.constructor.name === 'Object';
+}
+
+export function merge<T>(newer: T, older: T, mode: SetTemplateMode): T {
+  if (mode === 'replace') return newer;
+  if (isSugarObject(newer) && isSugarObject(older)) {
+    return { ...older, ...newer };
+  }
+  if (newer === null || newer === undefined) {
+    return older;
+  }
+  return newer;
 }
 
 export declare interface BetterObjectConstructor extends ObjectConstructor {
